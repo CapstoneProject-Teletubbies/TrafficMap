abda1870d3497504b4f303127ba61f969406a38b
sujin1018 <soojin_expo@naver.com>
2022-07-02 22:35:24 +0900

    api 받아오기 테스트

--- map/src/main/java/teletubbies/map/find/FindController.java
+++ map/src/main/java/teletubbies/map/find/FindController.java
@@ -1,20 +1,54 @@
 package teletubbies.map.find;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
+//TMAP APPKEY : l7xxafd79b2f4cce4ae6a4f6d1a1f4c31386
+
+@Slf4j
 @RestController
-@RequestMapping("/find")
+@RequestMapping("/api")
 public class FindController {
 
-    @GetMapping("/address")
-    public String address() {
-        System.out.println("지도API 테스트");
+    @Value("${TMAP_APPKEY}")
+    private String tmap_apiKey; //티맵 API 앱키 설정
 
-        return "address";
+    @GetMapping("/find/address")
+    public String tMapAPI() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://apis.openapi.sk.com/")
+                .path("tmap/pois")
+                .queryParam("version", 1) //version은 1
+                .queryParam("searchKeyword","스타벅스 부평점") //일단 스타벅스 부평점으로 검색
+                .queryParam("count",1) // 1개만 출력
+                .encode()
+                .build()
+                .toUri();
+
+        //RestTemplate : REST API 호출이후 응답을 받을 때까지 기다리는 동기방식
+        RestTemplate restTemplate = new RestTemplate();
+
+        //헤더를 넣기 위한 것
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("appKey", tmap_apiKey)
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
     }
 
 }
--- map/src/main/java/teletubbies/map/find/FindServiceImpl.java
+++ map/src/main/java/teletubbies/map/find/FindServiceImpl.java
@@ -1,5 +1,6 @@
 package teletubbies.map.find;
 
+import lombok.Value;
 import org.springframework.stereotype.Service;
 
 @Service
