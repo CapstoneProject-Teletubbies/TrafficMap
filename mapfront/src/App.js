2c0c5c94e85148d29c7f644aacb18c51c6041629
hwayoung <lweyine.sm@gmail.com>
2022-07-06 03:18:39 +0900

    초기화

--- mapfront/src/App.js
+++ mapfront/src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/main' element={<Main />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
