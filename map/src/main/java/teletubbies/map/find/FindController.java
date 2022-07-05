ebfa72c104f88f3476ed8087029bd35ecbf38951
sujin1018 <soojin_expo@naver.com>
2022-07-05 16:36:16 +0900

    엘리베이터 데이터 받아오기 추가

--- map/src/main/java/teletubbies/map/find/FindController.java
+++ map/src/main/java/teletubbies/map/find/FindController.java
@@ -2,18 +2,15 @@ package teletubbies.map.find;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.IOException;
 import java.net.URI;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @RestController
@@ -23,8 +20,16 @@ public class FindController {
     @Value("${TMAP_APPKEY}")
     private String tmap_apiKey; //티맵 API 앱키 설정
 
+    @Value("${ELEVATOR_APPKEY}")
+    private String elevator_apikey; //엘리베이터 API 키 설정
+
+     //(티맵) 명칭(POI) 통합 검색
     @GetMapping("/find/address")
     public String tMapAPI() {
+        //RestTemplate : REST API 호출이후 응답을 받을 때까지 기다리는 동기방식
+        RestTemplate restTemplate = new RestTemplate();
+
+        //URI 생성
         URI uri = UriComponentsBuilder
                 .fromUriString("https://apis.openapi.sk.com/")
                 .path("tmap/pois")
@@ -35,18 +40,34 @@ public class FindController {
                 .build()
                 .toUri();
 
-        //RestTemplate : REST API 호출이후 응답을 받을 때까지 기다리는 동기방식
-        RestTemplate restTemplate = new RestTemplate();
-
         //헤더를 넣기 위한 것
         RequestEntity<Void> req = RequestEntity
                 .get(uri)
-                .header("appKey", tmap_apiKey)
+                .header("appKey", tmap_apiKey) //앱키
                 .build();
 
         ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+//        System.out.println("result = " + result);
 
         return result.getBody();
+    }
+
+    // 주소로 엘리베이터 검색
+    @GetMapping("/find/elevator")
+    public Object elevatorAPI() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders(); //헤더
+
+        UriComponents uri = UriComponentsBuilder
+                .fromHttpUrl("http://openapi.elevator.go.kr/openapi/service/ElevatorOperationService/getOperationInfoList")
+                .queryParam("serviceKey", elevator_apikey) //서비스키
+                .queryParam("buld_address", "인천광역시 부평구 부평문화로 35") //주소
+                .queryParam("numOfRows", 1) // 개수
+                .queryParam("pageNo", 3)
+                .build(false);
+
+        Object response = restTemplate.exchange(uri.toUriString(), HttpMethod.GET, new HttpEntity<String>(headers), String.class);
+        return response;
     }
 
 }
--- map/src/test/java/teletubbies/map/MapApplicationTests.java
+++ map/src/test/java/teletubbies/map/MapApplicationTests.java
@@ -1,7 +1,16 @@
 package teletubbies.map;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.UnsupportedEncodingException;
 
 @SpringBootTest
 class MapApplicationTests {
@@ -10,4 +19,26 @@ class MapApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void apiTest() throws UnsupportedEncodingException {
+
+		//http://openapi.elevator.go.kr/openapi/service/ElevatorOperationService
+		String serviceKey="서비스키";
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+
+		//String decodeServiceKey = URLDecoder.decode(serviceKey, "UTF-8");
+		UriComponents uri = UriComponentsBuilder
+				.fromHttpUrl("http://openapi.elevator.go.kr/openapi/service/ElevatorOperationService/getOperationInfoList")
+				.queryParam("serviceKey",serviceKey)
+				.queryParam("buld_address","인천광역시 부평구 부평문화로 35") //모다 부평점
+				.queryParam("numOfRows",1) // 1개만 출력
+				.queryParam("pageNo",1)
+				.build(false);
+		//System.out.println(uri);
+
+		Object response = restTemplate.exchange(uri.toUriString(), HttpMethod.GET, new HttpEntity<String>(headers), String.class);
+		System.out.println(response);
+	}
 }
