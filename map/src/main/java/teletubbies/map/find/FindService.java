d2121fb5dba1cf270ca4f31814bb1e429eb02950
sujin1018 <soojin_expo@naver.com>
2022-07-06 21:51:09 +0900

    자료형 맞추기

--- map/src/main/java/teletubbies/map/find/FindService.java
+++ map/src/main/java/teletubbies/map/find/FindService.java
@@ -2,5 +2,5 @@ package teletubbies.map.find;
 
 public interface FindService {
     String findAddressByTmapAPI(String name); // 티맵 API로 명칭 검색
-    Object findElevatorByAPI(String address); // 엘리베이터 API로 엘리베이터 정보 가져오기
+    String findElevatorByAPI(String address); // 엘리베이터 API로 엘리베이터 정보 가져오기
 }
