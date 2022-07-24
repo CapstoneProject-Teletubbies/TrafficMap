eb427f2dcf919ed9c3b0bd24d68a4aac9a4a548c
origin <alicx991117@naver.com>
2022-07-24 16:18:37 +0900

    엘레베이터 속도 개선

--- map/src/test/java/teletubbies/map/MapApplicationTests.java
+++ map/src/test/java/teletubbies/map/MapApplicationTests.java
@@ -23,6 +23,8 @@ import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
 
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.time.Duration;
 import java.util.function.Consumer;
@@ -63,7 +65,7 @@ class MapApplicationTests {
 	@Test
 	void apiTestWebClient() throws UnsupportedEncodingException {
 
-
+		long start = System.currentTimeMillis();
 		DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory("http://openapi.elevator.go.kr/openapi/service/ElevatorOperationService");
 		factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
 
@@ -71,15 +73,23 @@ class MapApplicationTests {
 
 		String encodedName = URLEncoder.encode("인천광역시 부평구 부평문화로 35","UTF-8");
 
-		String response = wc.get()
+		Mono<String> response = wc.get()
 				.uri(uriBuilder -> uriBuilder.path("/getOperationInfoList")
 						.queryParam("serviceKey",elevator_apikey)
 						.queryParam("buld_address",encodedName) //모다 부평점
 						.queryParam("numOfRows",1) // 1개만 출력
 						.queryParam("pageNo",1).build())
-				.retrieve().bodyToMono(String.class).block();
+				.retrieve().bodyToMono(String.class);//.block();
+		long end = System.currentTimeMillis();
+		System.out.println("걸리는 시간 : " + (end - start)/1000.0);
 
-		System.out.println(response);
+		long start1 = System.currentTimeMillis();
+		response.subscribe( (data)->{
+			System.out.println(data);
+		});
+		long end1 = System.currentTimeMillis();
+
+		System.out.println("걸리는 시간 : " + (end1 - start1)/1000.0);
 
 		/*
 		Mono<String> mono = (Mono<String>) WebClient.builder().baseUrl("http://openapi.elevator.go.kr/openapi/service/ElevatorOperationService")
@@ -134,13 +144,13 @@ class MapApplicationTests {
 		//response.doOnNext(Thread.sleep(1000)).doOnSuccess(value -> System.out.println("success1"+value));
 
 		//Timeout.millis(1000);
-
+/*
 		response.subscribe(
 				value -> System.out.println("결과: "+value)//,
 				//error -> error.printStackTrace(),
 				//() -> System.out.println("completed without a value")
 		);
-
+*/
 
 
 
@@ -204,6 +214,34 @@ class MapApplicationTests {
 
 
 	}
+
+///*
+//	@Test
+//	void httpApi(){
+//		StringBuffer result = new StringBuffer();
+//
+//
+//		.uri(uriBuilder -> uriBuilder.path("/getOperationInfoList")
+//						.queryParam("serviceKey",elevator_apikey)
+//						.queryParam("buld_address",encodedName) //모다 부평점
+//						.queryParam("numOfRows",1) // 1개만 출력
+//						.queryParam("pageNo",1).build())
+//
+//		String encodedName = URLEncoder.encode("인천광역시 부평구 부평문화로 35","UTF-8");
+//
+//
+//		String encodedName = URLEncoder.encode("인천광역시 부평구 부평문화로 35","UTF-8");
+//		try{
+//			String urlstr = "http://openapi.elevator.go.kr/openapi/service/ElevatorOperationService/getOperationInfoList"
+//					+"?serviceKey="+elevator_apikey
+//					+"&buld_address="+encodedName
+//					+"&numOfRow=1&pageNo=1";
+//			URL url=new URL(urlstr);
+//			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//
+//		}
+//	}
+//	*/
 
 
 }
