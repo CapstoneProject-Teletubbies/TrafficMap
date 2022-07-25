3494a28d9199148b415b3436bca69c1f8eca0444
박수진 <soojin_expo@naver.com>
2022-07-25 19:06:05 +0900

    오류 수정

--- map/src/main/java/teletubbies/map/bus/BusRouteDetailDto.java
+++ map/src/main/java/teletubbies/map/bus/BusRouteDetailDto.java
@@ -7,8 +7,8 @@ public class BusRouteDetailDto {
     public Integer ROUTEID; // 버스 노선 고유번호
     public Object ROUTENO; // 노선 명
     public Integer ROUTETPCD; // 노선유형코드 [1:지선형, 2:간선형, 3:좌석형, 4:광역형, 5:리무진, 6:마을버스, 7:순환형, 8:급행간선, 9:지선(순환)]
-    public String FBUS_DEPHMS; // 첫차 시간 hhmm
-    public String LBUS_DEPHMS; // 막차 시간 hhmm
+    public Integer FBUS_DEPHMS; // 첫차 시간 hhmm
+    public Integer LBUS_DEPHMS; // 막차 시간 hhmm
     public  Integer MIN_ALLOCGAP; // 	최소 배차간격
     public Integer MAX_ALLOCGAP; // 최대 배차간격
     public Integer TURN_BSTOPID; // 회차지 정류소ID
