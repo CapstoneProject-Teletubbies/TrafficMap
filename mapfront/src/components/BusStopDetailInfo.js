adc1dc94715c54d0184caf318361d49d42df1ed6
hwayoung <lweyine.sm@gmail.com>
2022-08-17 23:00:06 +0900

    버스 도착 시간 추가

--- mapfront/src/components/BusStopDetailInfo.js
+++ mapfront/src/components/BusStopDetailInfo.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 import { useRef, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Draggable from 'react-draggable';
+import {DraggableCore} from 'react-draggable';
 import proj4 from 'proj4';
 import axios from 'axios';
 import BusInfo from '../components/BusInfo'
@@ -56,10 +57,12 @@ const BusStopDetailInfo = (props)=>{
         var busarr = props.bustop;
         console.log(busarr);
         console.log("useEffect");
+        if(busarr){
         busarr.map((obj, index) => {
             searchBusNum(obj.routeid);
         });
         SetIsSet(true);
+        }
     }, [])
 
     useEffect(()=>{
@@ -67,16 +70,35 @@ const BusStopDetailInfo = (props)=>{
         setRBus(props.bustop);
         
         setTimeout((bustopinfobar = document.getElementById('bustopinfobar')), 100);
-        // setTimeout(()=>{
-        //     rbuslist = document.querySelector('#rbuslist');
-        //     rbuslist.addEventListener('click', oo());
-        // }, 100);
+        setTimeout(()=>{
+            $('#rbuslist').on('touchstart', function(e){
+                setDisable(true);
+                console.log(e.changedTouches[0].clientX);
+                console.log("먼가시작");
+            });
+            $('#rbuslist').on('touchmove', function(e){
+                setDisable(true);
+                console.log("먼가ing");
+            });
+            $('#rbuslist').on('touchend', function(e){
+                setDisable(false);
+                console.log("먼가끝");
+            });
+        }, 50);
         var height = -(bustopinfobar.offsetHeight*0.9);
         setTop(height);
     }, [])
 
-    function oo(){
+    function touchstart(){
         console.log("터치요");
+    }
+
+    function touchmove(){
+        console.log("터치중~");
+    }
+
+    function touchend(){
+        console.log("터치끝");
     }
 
     ////////////// 현재 내 위치와 선택한 건물의 거리 계산 (버정 BESSELTM 좌표계라 변환하는거까지)
@@ -111,12 +133,13 @@ const BusStopDetailInfo = (props)=>{
     }, [])
     //////////////
 
-    const handleDragStart = (evt) => {          //드래그 시작 이벤트
+    const handleDragStart = (evt) => {          //드래그 시작 이벤트  
         setStartXY(evt);    
     }
 
-    const handleDrag = (evt) => {               //드래그 중 이벤트
-        console.log("드래그중~")
+    const handleDrag = (evt) => {   //드래그 중 이벤트           
+        console.log("드래그중~");
+
     }
 
     const handleDragStop = (evt) => {                           //드래그 끝났을 때 이벤트
@@ -129,28 +152,32 @@ const BusStopDetailInfo = (props)=>{
             $('#bustopinfobar').stop(true).animate({bottom: posy}, 300);
             setTop(evt.lastY);
             setBottom(height);
+            // setDisable(true);
         }
         else if(posy < 0){   
             var height = -(element.offsetHeight)*0.87 + evt.lastY;
             setTop(height);
             setBottom(evt.lastY + 13);
             $('#bustopinfobar').stop(true).animate({bottom: height}, 300);
+            // setDisable(false);
         }
+
     }
 
-    const test = () => {            //출발, 도착 터치 이벤트
+    const test = (evt) => {            //출발, 도착 터치 이벤트
         console.log("클릭");
+        
     }
  
     return(
-        <Draggable ref={ref} disabled={disable} onStart={(e, data) => handleDragStart(data)} onDrag={(e, data) => handleDrag(data)} onStop={(e, data) => handleDragStop(data)} axis='y' bounds={{top: top, bottom: bottom}}>
+        <Draggable ref={ref} onMouseDown={(e)=>{test(e)}} disabled={disable} onStart={(e, data) => handleDragStart(data)} onDrag={(e, data) => handleDrag(data)} onStop={(e, data) => handleDragStop(data)} axis='y' bounds={{top: top, bottom: bottom}}>
         <div id="bustopinfobar" style={{position: "fixed", backgroundColor: "white", width: "100%", height: "100%", bottom: "-87%", borderRadius: "7px", textAlign: "-webkit-center",
                 boxShadow: "0px 2px 20px 2px #A6A6A6"}}>
             <div style={{position: "relative", backgroundColor: "#D5D5D5", width: "50%", height: "0.6%",  marginTop: "6px"
                         , borderRadius: "6px", }}>
             </div>
                 {bustop &&
-                    <div style={{position: "relative"}}>
+                    <div style={{position: "relative", height: "100%"}}>
                         <div style={{position: "fixed", width:"100%", height: "12%"}}>
                             <div style={{fontSize: "1.2rem", float: "left", padding: "9px"}}>
                                 {bustop.bstopnm} <br></br>
@@ -161,21 +188,22 @@ const BusStopDetailInfo = (props)=>{
                                 <button id="button2" onTouchEnd={test} type="button" class="btn btn-primary btn-sm col-5" style={{borderRadius: "20px", height: "35px", marginLeft: "8px"}}>도착</button>
                             </div>
                         </div>
-                        <div id="rbuslist" style={{position: "fixed", overflowY: "scroll", width: "100%", height: "87%", bottom: "0px", boxShadow: "0px 1px 1px 1px gray"}}>
+                        <div id="rbuslist" onTouchStart={touchstart} onTouchMove={touchmove} onTouchEnd={touchend} style={{position: "fixed", overflowY: "scroll", width: "100%", height: "87%", bottom: "0px", boxShadow: "0px 1px 1px 1px gray", zIndex: "6"}}>
                             <ol className="list-group" >
                             {rbus && isset && rbus.map((obj, index)=>{
                                 var test = busnumlist[index];
                                 if(test){
+                                    let time = parseInt(obj.arrivalestimatetime/60);
                                 return(
                                     <div>
                                     <BusInfo obj={test}></BusInfo>
-                                    <div>
-                                        <h2>test</h2>
+                                    <div style={{float: "left", textAlign: "-webkit-left" ,width: "100%", paddingLeft: "5%", boxShadow: "inset 0px 0px 1px 0.1px gray"}}>
+                                        남은시간: {time}분
                                     </div>
                                     </div>
                                 );}})}
                             </ol>
-                        </div>      
+                        </div>     
                     </div>
                 }
         </div>
